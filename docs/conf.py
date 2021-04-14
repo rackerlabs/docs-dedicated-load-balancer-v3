f65fde98b89b6137a1a004c386c2a5e364c1ea8b
Ravi <raviteja.valluri@rackspace.com>
2021-04-14 11:42:20 -0700

    HTML for Search, Header and Footer

--- docs/conf.py
+++ docs/conf.py
@@ -196,7 +196,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Rackspace Technology Dedicated Load Balancer v3 API Guide'
+# html_title = 'Rackspace Technology Dedicated Load Balancer v3 API Guide'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = 'Rackspace Dedicated Load Balancers'
